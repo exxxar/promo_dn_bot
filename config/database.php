fd3bc188416498a10e6a5be02fedc0276cb87494
Анонимный Аноним <exxxar@users.noreply.github.com>
2019-11-27 10:20:29 +0300

    try deploy on heroku

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,11 +56,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'ec2-54-228-243-238.eu-west-1.compute.amazonaws.com'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'd3845kmgo10dcq'),
+            'username' => env('DB_USERNAME', 'vrawpxxujbruhc'),
+            'password' => env('DB_PASSWORD', '60d7b8dcaade34d2ca2b2a52fa1a954447e1d9d5192158ca1f2fee14698c13b'),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
