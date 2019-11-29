6686d357953b81ffdb2e2daa524ae1b555857bc4
Анонимный Аноним <exxxar@users.noreply.github.com>
2019-11-29 23:31:38 +0300

    test

--- app/Http/Controllers/BotManController.php
+++ app/Http/Controllers/BotManController.php
@@ -6,7 +6,6 @@ use App\Conversations\PaymentConversation;
 use App\Conversations\PromoConversation;
 use App\Conversations\StartConversation;
 use App\Conversations\StartDataConversation;
-use App\Conversations\StopConversation;
 use BotMan\BotMan\BotMan;
 use Illuminate\Http\Request;
 
@@ -42,10 +41,10 @@ class BotManController extends Controller
         $bot->startConversation(new StartConversation($bot));
     }
 
-    public function stopConversation(BotMan $bot)
+   /* public function stopConversation(BotMan $bot)
     {
         $bot->startConversation(new StopConversation($bot));
-    }
+    }*/
 
     public function startDataConversation(BotMan $bot,$data)
     {
