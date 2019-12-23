7869b498b251aa4b5ac9d1d307bd06d6e9ba047d
exxxar <exxxar@gmail.com>
2019-12-23 18:08:51 +0300

    добавил раздел статьи

--- resources/views/admin/articles/edit.blade.php
+++ resources/views/admin/articles/edit.blade.php
@@ -66,10 +66,14 @@
                             <td>
 
 
-                                <input type="radio" name="is_visible" value="0" required id="is_visible_1">
+                                <input type="radio" name="is_visible" value="0" required
+                                       {{!$article->is_visible?checked:''}}
+                                       id="is_visible_1">
                                 <label for="is_visible_1">Не отображать</label>
 
-                                <input  type="radio" name="is_visible" value="1" required id="is_visible_2">
+                                <input  type="radio" name="is_visible" value="1" required
+                                        {{$article->is_visible?checked:''}}
+                                        id="is_visible_2">
                                 <label for="is_visible_2">Отображать</label>
 
 
