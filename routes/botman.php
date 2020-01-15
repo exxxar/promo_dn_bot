f76bc4bafe233e5bcd6b5d57f20e4585d738a023
exxxar <exxxar@gmail.com>
2020-01-15 10:46:05 +0300

    test

--- routes/botman.php
+++ routes/botman.php
@@ -1315,7 +1315,6 @@ $botman->hears('/check_lottery_slot ([0-9]+) ([0-9]+)', function ($bot, $slotId,
     }
 });
 
-$botman->hears('/test_controller', 'BotController@test');
 
 $botman->fallback(function ($bot) {
     $bot->loadDriver(TelegramInlineQueryDriver::DRIVER_NAME);
