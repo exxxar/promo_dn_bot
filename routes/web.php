97fbec3789d8b21e1341218aa86a4984292753bd
exxxar <exxxar@gmail.com>
2020-01-29 22:05:32 +0300

    правки по связыванию мероприятия с акцией

--- routes/web.php
+++ routes/web.php
@@ -191,7 +191,58 @@ Route::get("/insta", function (Request $request) {
     //
     ////17841407882850175?fields=mentioned_media.media_id(18040865266238470){caption,media_type,username}   - получаем инфу о пользователе по идентификации медиа-объекта
     //
+    $requestUserName = $fb->request('GET', '/me?fields=id,name');
 
+// Get user likes
+    $requestUserLikes = $fb->request('GET', '/me/likes?fields=id,name&amp;limit=1');
+
+// Get user events
+    $requestUserEvents = $fb->request('GET', '/me/events?fields=id,name&amp;limit=2');
+
+// Post a status update with reference to the user's name
+    $message = 'My name is {result=user-profile:$.name}.' . "\n\n";
+    $message .= 'I like this page: {result=user-likes:$.data.0.name}.' . "\n\n";
+    $message .= 'My next 2 events are {result=user-events:$.data.*.name}.';
+    $statusUpdate = ['message' => $message];
+    $requestPostToFeed = $fb->request('POST', '/me/feed', $statusUpdate);
+
+// Get user photos
+    $requestUserPhotos = $fb->request('GET', '/me/photos?fields=id,source,name&amp;limit=2');
+
+    $batch = [
+        'user-profile' => $requestUserName,
+        'user-likes' => $requestUserLikes,
+        'user-events' => $requestUserEvents,
+        'post-to-feed' => $requestPostToFeed,
+        'user-photos' => $requestUserPhotos,
+    ];
+
+    echo '<h1>Make a batch request</h1>' . "\n\n";
+
+    try {
+        $responses = $fb->sendBatchRequest($batch);
+    } catch(Facebook\Exceptions\FacebookResponseException $e) {
+        // When Graph returns an error
+        echo 'Graph returned an error: ' . $e->getMessage();
+        exit;
+    } catch(Facebook\Exceptions\FacebookSDKException $e) {
+        // When validation fails or other local issues
+        echo 'Facebook SDK returned an error: ' . $e->getMessage();
+        exit;
+    }
+
+    foreach ($responses as $key => $response) {
+        if ($response->isError()) {
+            $e = $response->getThrownException();
+            echo '<p>Error! Facebook SDK Said: ' . $e->getMessage() . "\n\n";
+            echo '<p>Graph Said: ' . "\n\n";
+            var_dump($e->getResponse());
+        } else {
+            echo "<p>(" . $key . ") HTTP status code: " . $response->getHttpStatusCode() . "<br />\n";
+            echo "Response: " . $response->getBody() . "</p>\n\n";
+            echo "<hr />\n\n";
+        }
+    }
 
 });
 
