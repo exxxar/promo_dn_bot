fe78d348fc7f5e1bfa588de037b0b4dc9f4660d3
exxxar <exxxar@gmail.com>
2020-01-31 13:50:01 +0300

    правки по стилю мероприятий

--- resources/views/admin/events/index.blade.php
+++ resources/views/admin/events/index.blade.php
@@ -30,57 +30,51 @@
                     </div>
                 </div>
 
-                    <h1>События\мероприятия</h1>
+                <h1>События\мероприятия</h1>
                 @isset($events)
-                    <table class="table mt-2">
 
-                        <thead class="thead-light">
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Заголовок</th>
-                            <th scope="col">Описание</th>
-                            <th scope="col">Действие</th>
+                    @foreach($events as $key => $event)
 
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @foreach($events as $key => $event)
-                            <tr>
-                                <td>{{$key + 1}}</td>
-                                <td><a href="{{ route('events.show',$event->id) }}">
-                                        {{$event->title}}</a>
-                                    <a class="btn btn-link" href="{{ route('events.edit',$event->id) }}">
-                                        <i class="fas fa-edit"></i>
+
+                        <div class="card card-accent-success" style="width: 200px">
+                            <div class="card-header">
+                                <a class="btn btn-link" href="{{ route('events.edit',$event->id) }}">
+                                    <i class="fas fa-edit"></i>
+                                </a>
+                                <a class="btn btn-link" href="{{ route('events.channel',$event->id) }}"
+                                   title="Отправить в канал">
+                                    <i class="fab fa-telegram"></i>
+                                </a>
+
+                                @if($event->promo_id!=null)
+                                    <a class="btn btn-link" href="{{ route('promotions.show',$event->promo_id) }}"
+                                       title="Связано с акцией">
+                                        <i class="fas fa-link"></i>
                                     </a>
+                                @endif
+                                <form class="btn btn-link" action="{{ route('events.destroy', $event->id)}}"
+                                      method="post">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="btn btn-link" type="submit"><i class="fas fa-times"></i></button>
+                                </form>
+                            </div>
+                            <img class="card-img-top" src="..." alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">{{$event->title}}</h5>
+                                <p class="card-text">{{$event->description}}</p>
+                                <a class="btn btn-primary" href="{{ route('events.show',$event->id) }}">
+                                    {{$event->title}}</a>
+                            </div>
+                        </div>
 
-                                </td>
-                                <td>{{$event->description}}</td>
-
-
-                                <td>
-                                    <a class="btn btn-link" href="{{ route('events.channel',$event->id) }}" title="Отправить в канал">
-                                        <i class="fab fa-telegram"></i>
-                                    </a>
-
-                                    @if($event->promo_id!=null)
-                                        <a class="btn btn-link" href="{{ route('promotions.show',$event->promo_id) }}" title="Связано с акцией">
-                                            <i class="fas fa-link"></i>
-                                        </a>
-                                        @endif
-                                    <form action="{{ route('events.destroy', $event->id)}}" method="post">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button class="btn btn-link" type="submit"><i class="fas fa-times"></i></button>
-                                    </form>
-                                </td>
-                            </tr>
                         @endforeach
 
                         </tbody>
-                    </table>
+                        </table>
 
-                    {{ $events->links() }}
-                @endisset
+                        {{ $events->links() }}
+                        @endisset
             </div>
         </div>
     </div>
