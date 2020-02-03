2037ff7ff714ddf457618fbfcd5133e6b78e46bd
exxxar <exxxar@gmail.com>
2020-02-03 23:26:01 +0300

    правки по авторизации

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -34,12 +34,14 @@
                                     <input class="form-control" type="password" placeholder="{{ __('Пароль') }}"
                                            name="password" required>
                                 </div>
-                                <div class="row">
-                                    <div class="col-6">
+                                <div class="row justify-content-center">
+                                    <div class="col-12">
 
                                         <button class="btn btn-primary px-4" type="submit">{{ __('Войти') }}</button>
                                     </div>
-                                    <div class="col-6">
+                                </div>
+                                <div class="row justify-content-center">
+                                    <div class="col-12">
                                         <script async src="https://telegram.org/js/telegram-widget.js?7" data-telegram-login="skidki_dn_bot" data-size="medium" data-radius="5" {{--data-onauth="onTelegramAuth(user)"--}} data-auth-url="https://skidka-service.ru/auth/telegram/callback" data-request-access="write"></script>
 
                                     </div>
