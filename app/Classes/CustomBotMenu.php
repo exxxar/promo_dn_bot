f9ed092d2590ba9da2c75a64a2fe2a3e9875df35
exxxar <exxxar@gmail.com>
2020-02-08 23:34:36 +0300

    правки

--- app/Classes/CustomBotMenu.php
+++ app/Classes/CustomBotMenu.php
@@ -40,7 +40,7 @@ trait CustomBotMenu
     {
         $this->bot = $bot;
         if (env("MAINTENANCE_MODE") === true) {
-            $this->fallbackMenu("Система находится на техническом обслуживании! Ориентировочное время *" . env("MAINTENANCE_TIME") . " мин.*");
+            $this->fallbackMenu("Система находится на техническом обслуживании! Ориентировочное время " . env("MAINTENANCE_TIME") . " мин.");
             $this->sendPhoto("*Пока мы улучшаем наши сервисы Вы можете узнать про актаульные акции и скидки в нашем канале!*",
                 "https://sun9-29.userapi.com/c858232/v858232349/173635/lTlP7wMcZEA.jpg",
                 [
