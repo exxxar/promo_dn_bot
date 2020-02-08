59ed5cbf004d306d154dcac5ec485ad65b900b3b
exxxar <exxxar@gmail.com>
2020-02-08 23:05:22 +0300

    правки

--- app/Classes/CustomBotMenu.php
+++ app/Classes/CustomBotMenu.php
@@ -41,7 +41,7 @@ trait CustomBotMenu
         $this->bot = $bot;
         if (env("MAINTENANCE_MODE")===true) {
             $this->fallbackMenu("Система находится на техническом обслуживании! Ориентировочное время 10 минут.");
-            return null;
+            return $this;
         }
         $this->createNewUser();
         return $this;
