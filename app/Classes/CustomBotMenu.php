b4a3814c9db72cc6a4b9ad6e726c4492d6d10721
exxxar <exxxar@gmail.com>
2020-02-09 12:42:54 +0300

    правки по меню помощи

--- app/Classes/CustomBotMenu.php
+++ app/Classes/CustomBotMenu.php
@@ -36,12 +36,13 @@ trait CustomBotMenu
 
     public function initKeyboards()
     {
-        /*$this->keyboard = [
+        $this->keyboard = [
             [__("messages.global_menu_3"), __("messages.global_menu_5")],
             [__("messages.global_menu_1"), __("messages.global_menu_2")],
-            [__("messages.global_menu_4")]
-        ];*/
+            [  "\xF0\x9F\x93\x9CПомощь"]
+        ];
 
+        /*
         $this->keyboard = [
             [
                 "\xE2\xAD\x90Акции, скидки и мероприятия"
@@ -57,7 +58,7 @@ trait CustomBotMenu
             ],
 
         ];
-
+*/
         $this->keyboard_promotions = [
             [
                 "Акции по компаниям", "Акции по категориям",
