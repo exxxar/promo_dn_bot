196888608b139cb5e0c4a406384b4e97bfdb1e11
exxxar <exxxar@gmail.com>
2020-02-14 00:56:00 +0300

    правки по начислению cashback-a

--- app/Listeners/CashBackHandler.php
+++ app/Listeners/CashBackHandler.php
@@ -12,6 +12,7 @@ use App\Events\NetworkCashBackEvent;
 use App\User;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\Log;
 use Telegram\Bot\Laravel\Facades\Telegram;
 
 class CashBackHandler
@@ -34,46 +35,50 @@ class CashBackHandler
      */
     public function handle(AddCashBackEvent $event)
     {
-        $user = User::find($event->user_id);
-        $company = Company::find($event->company_id);
-        $cashBack = round(intval($event->money_in_check) * $company->cashback / 100);
-        $companyName = $company->title ?? "Неизвестная компания";
+        try {
+            $user = User::find($event->user_id);
+            $company = Company::find($event->company_id);
+            $cashBack = round(intval($event->money_in_check) * $company->cashback / 100);
+            $companyName = $company->title ?? "Неизвестная компания";
 
-        if (env("INDIVIDUAL_CASHBACK_MODE") == false) {
-            $user->cashback_bonus_count += $cashBack;
-            $user->save();
-        } else
-            if (env("INDIVIDUAL_CASHBACK_MODE") == true) {
-                $cbi = CashBackInfo::where("company_id", $event->company_id)
-                    ->where("user_id", $event->user_id)
-                    ->first();
+            if (env("INDIVIDUAL_CASHBACK_MODE") == false) {
+                $user->cashback_bonus_count += $cashBack;
+                $user->save();
+            } else
+                if (env("INDIVIDUAL_CASHBACK_MODE") == true) {
+                    $cbi = CashBackInfo::where("company_id", $event->company_id)
+                        ->where("user_id", $event->user_id)
+                        ->first();
 
-                if (is_null($cbi)) {
-                    CashBackInfo::create([
-                        'user_id' => $event->user_id,
-                        'company_id' => $event->company_id,
-                        'value' => $cashBack
-                    ]);
-                } else {
-                    $cbi->value += $cashBack;
-                    $cbi->save();
+                    if (is_null($cbi)) {
+                        CashBackInfo::create([
+                            'user_id' => $event->user_id,
+                            'company_id' => $event->company_id,
+                            'value' => $cashBack
+                        ]);
+                    } else {
+                        $cbi->value += $cashBack;
+                        $cbi->save();
+                    }
                 }
-            }
 
-        event(new ActivateUserEvent($user));
-        event(new NetworkCashBackEvent($event->user_id, $cashBack));
-        event(new AchievementEvent(
-                AchievementTriggers::MaxCashBackCount,
-                $cashBack,
-                $user
-            )
-        );
+            event(new ActivateUserEvent($user));
+            event(new NetworkCashBackEvent($event->user_id, $cashBack));
+            event(new AchievementEvent(
+                    AchievementTriggers::MaxCashBackCount,
+                    $cashBack,
+                    $user
+                )
+            );
 
-        Telegram::sendMessage([
-            'chat_id' => $user->telegram_chat_id,
-            'parse_mode' => 'Markdown',
-            'text' => "Сумма в чеке *$event->money_in_check* руб.\nВам начислен *CashBack* в размере *$cashBack* руб от компании *$companyName*",
-            'disable_notification' => 'false'
-        ]);
+            Telegram::sendMessage([
+                'chat_id' => $user->telegram_chat_id,
+                'parse_mode' => 'Markdown',
+                'text' => "Сумма в чеке *$event->money_in_check* руб.\nВам начислен *CashBack* в размере *$cashBack* руб от компании *$companyName*",
+                'disable_notification' => 'false'
+            ]);
+        } catch (\Exception $e) {
+            Log::info($e->getMessage() . " " . $e->getLine());
+        }
     }
 }
