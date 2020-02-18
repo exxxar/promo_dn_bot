efcb40dfe0b7140cf3fecae2ab1fc1a6e000d95a
exxxar <exxxar@gmail.com>
2020-02-18 13:04:47 +0300

    проверяем возможность редактирования сообщений

--- app/Classes/CustomBotMenu.php
+++ app/Classes/CustomBotMenu.php
@@ -296,7 +296,7 @@ trait CustomBotMenu
 
         Telegram::editMessageCaption([
             'chat_id' => $this->getChatId(),
-            "message_id" => $messageId,
+            "message_id" => $messageId-1,
             "caption" => "TEST TEST",
             'reply_markup' => json_encode([
                 'inline_keyboard' => $keyboard,
