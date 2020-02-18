3de00c9fb0ecde0275b799856db8bae4e6e511ff
exxxar <exxxar@gmail.com>
2020-02-18 14:40:35 +0300

    проверяем возможность редактирования сообщений

--- routes/botman.php
+++ routes/botman.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Log;
 use Telegram\Bot\Laravel\Facades\Telegram;
 
 
-$botman = resolve('botman');
+$botman = BotManFactory::create(config('botman'));//resolve('botman');
 
 
 $botman->hears(".*Ввести код и начать|/lottery", BotController::class . '@lotteryConversation');
