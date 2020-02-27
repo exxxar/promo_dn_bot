88acb3a2f09614310e9700103b6098e49f7653d0
exxxar <exxxar@gmail.com>
2020-02-27 16:09:45 +0300

    правки по главной странице админки

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8">
+            <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">Панель управления</div>
 
@@ -27,19 +27,36 @@
                             </div>
                         @endif
 
-                       {{-- @isset($instaURL)
-                                <a href="{{$instaURL}}">Обработать данные из Instagram</a>
-                        @endisset--}}
-                        @include("partials.cashback")
-                        <hr>
-                        @include("partials.search")
-                        <hr>
-                        @include("partials.announce")
-                        <hr>
-                        @include("partials.generate")
+                        <div class="nav-tabs-boxed">
+                            <ul class="nav nav-tabs" role="tablist">
+                                <li class="nav-item"><a class="nav-link active" data-toggle="tab" href="#home"
+                                                        role="tab" aria-controls="home">Меню</a></li>
+                                <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#cashback" role="tab"
+                                                        aria-controls="cashback">Начисления CashBack</a></li>
+                                <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#announce" role="tab"
+                                                        aria-controls="announce">Рассылки</a></li>
+                                <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#search" role="tab"
+                                                        aria-controls="search">Поиск</a></li>
+                                <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#generate" role="tab"
+                                                        aria-controls="generate">Генерация QR</a></li>
+                            </ul>
+                            <div class="tab-content">
+                                <div class="tab-pane active" id="home" role="tabpanel">1. Lorem ipsum dolor sit amet,
+                                    consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                                    magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                                    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                                    voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                                    cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+                                    laborum.
+                                </div>
+                                <div class="tab-pane" id="cashback" role="tabpanel"> @include("partials.cashback")</div>
+                                <div class="tab-pane" id="announce" role="tabpanel"> @include("partials.announce")</div>
+                                <div class="tab-pane" id="search" role="tabpanel"> @include("partials.search")</div>
+                                <div class="tab-pane" id="generate" role="tabpanel"> @include("partials.generate")</div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 @endsection
