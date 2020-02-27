9459e823ac31c102b7eda269301cf3a2add6247d
exxxar <exxxar@gmail.com>
2020-02-27 16:18:06 +0300

    правки по главной странице админки

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -29,8 +29,6 @@
 
                         <div class="nav-tabs-boxed">
                             <ul class="nav nav-tabs" role="tablist">
-                                <li class="nav-item"><a class="nav-link active" data-toggle="tab" href="#home"
-                                                        role="tab" aria-controls="home">Меню</a></li>
                                 <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#cashback" role="tab"
                                                         aria-controls="cashback">Начисления CashBack</a></li>
                                 <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#announce" role="tab"
@@ -41,14 +39,6 @@
                                                         aria-controls="generate">Генерация QR</a></li>
                             </ul>
                             <div class="tab-content">
-                                <div class="tab-pane active" id="home" role="tabpanel">1. Lorem ipsum dolor sit amet,
-                                    consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
-                                    magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                                    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-                                    voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                                    cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-                                    laborum.
-                                </div>
                                 <div class="tab-pane" id="cashback" role="tabpanel"> @include("partials.cashback")</div>
                                 <div class="tab-pane" id="announce" role="tabpanel"> @include("partials.announce")</div>
                                 <div class="tab-pane" id="search" role="tabpanel"> @include("partials.search")</div>
