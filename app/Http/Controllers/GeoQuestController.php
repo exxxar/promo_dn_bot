660da94c47b15f143d613d3bf8b9ce8fbbe076ab
exxxar <exxxar@gmail.com>
2020-02-28 22:54:10 +0300

    правки по гео-квестам

--- app/Http/Controllers/GeoQuestController.php
+++ app/Http/Controllers/GeoQuestController.php
@@ -75,7 +75,7 @@ class GeoQuestController extends Controller
             'title' => $request->get('title') ?? '',
             'description' => $request->get('description') ?? '',
             'image_url' => $request->get('image_url') ?? '',
-            'is_active' => $request->get('is_active') ?? false,
+            'is_active' => $request->get('is_active') == "on",
             'promotion_id' => $request->get('promotion_id') ?? null,
             'company_id' => $request->get('company_id') ?? null,
             'reward_bonus' => $request->get('reward_bonus') ?? 0,
@@ -144,7 +144,7 @@ class GeoQuestController extends Controller
         $quest->title = $request->get("title") ?? '';
         $quest->description = $request->get("description") ?? '';
         $quest->image_url = $request->get("image_url") ?? '';
-        $quest->is_active = $request->get("is_active") ?? false;
+        $quest->is_active = $request->get("is_active") == "on";
         $quest->reward_bonus = $request->get("reward_bonus") ?? '';
         $quest->position = $request->get("position") ?? 0;
         $quest->start_at = $request->get("start_at") ?? Carbon::now('+3:00');
