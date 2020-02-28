b0255a161c33b12be89a00b6b436f85f0cd7ac4c
exxxar <exxxar@gmail.com>
2020-02-28 22:38:39 +0300

    правки по гео-квестам

--- app/Http/Controllers/GeoQuestController.php
+++ app/Http/Controllers/GeoQuestController.php
@@ -80,8 +80,8 @@ class GeoQuestController extends Controller
             'company_id' => $request->get('company_id') ?? null,
             'reward_bonus' => $request->get('reward_bonus') ?? 0,
             'position' => $request->get('position') ?? 0,
-            'start_at' => $request->get('start_at') ?? Carbon::now('+3:00'),
-            'end_at' => $request->get('end_at') ?? Carbon::now('+3:00'),
+            'start_at' => Carbon::createFromFormat('Y-m-d\TH:i', $request->get('start_at') ),//?? Carbon::now('+3:00'),
+            'end_at' => Carbon::createFromFormat('Y-m-d\TH:i', $request->get('end_at')) ,//?? Carbon::now('+3:00'),
          //   'created_at' => Carbon::now('+3:00'),
            // 'updated_at' => Carbon::now('+3:00'),
         ]);
