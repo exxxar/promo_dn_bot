4565eca7e6a5d6e31aab329fb4858fd78aabb63d
exxxar <exxxar@gmail.com>
2020-03-02 21:41:18 +0300

    добавление квестов в бота

--- resources/views/ar.blade.php
+++ resources/views/ar.blade.php
@@ -6,27 +6,30 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+
+    <script src="https://aframe.io/releases/0.6.0/aframe.min.js"></script>
+    <script src="https://jeromeetienne.github.io/AR.js/aframe/build/aframe-ar.js"></script>
 </head>
-<body tyle='margin : 0px; overflow: hidden;'>
-<!-- AR.js by @jerome_etienne - github: https://github.com/jeromeetienne/ar.js - info: https://medium.com/arjs/augmented-reality-in-10-lines-of-html-4e193ea9fdbf -->
 
 <body style='margin : 0px; overflow: hidden;'>
 <a-scene embedded arjs='sourceType: webcam;'>
-  {{--  <a-box position='0 0.5 0' material='opacity: 0.5;'></a-box>--}}
-   {{-- <a-marker preset='custom' type='pattern' url='{{asset('1.patt')}}'>
-        <a-image src="{{asset('1.png')}}" position="0 0 0" rotation="90 180 0 "/>
-    </a-marker>
-    <a-entity camera></a-entity>--}}
-
-    <a-marker-camera type='pattern' patternUrl='{{asset('1.patt')}}'>
+    <!-- handle marker with your own pattern -->
+    <a-marker type='pattern' patternUrl='1.patt'>
         <a-box position='0 0.5 0' material='color: red;'></a-box>
-    </a-marker-camera>
+    </a-marker>
 
+    <!-- handle marker with hiro preset -->
+    <a-marker preset='hiro'>
+        <a-box position='0 0.5 0' material='color: green;'></a-box>
+    </a-marker>
+
+    <!-- handle barcode marker -->
+    <a-marker type='barcode' value='5'>
+        <a-box position='0 0.5 0' material='color: blue;'></a-box>
+    </a-marker>
+
+    <!-- add a simple camera -->
     <a-entity camera></a-entity>
 </a-scene>
-
-<script src="https://aframe.io/releases/0.8.0/aframe.min.js"></script>
-<script src="https://cdn.rawgit.com/jeromeetienne/AR.js/1.6.0/aframe/build/aframe-ar.js"></script>
-
 </body>
 </html>
