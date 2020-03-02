c0507e6ca64b76c6f4e22a4879ae83df4d3e406b
exxxar <exxxar@gmail.com>
2020-03-02 13:18:16 +0300

    geo positions

--- app/Models/SkidkaServiceModels/GeoQuest.php
+++ app/Models/SkidkaServiceModels/GeoQuest.php
@@ -44,7 +44,6 @@ class GeoQuest extends Model
 
     public function positions()
     {
-        return $this->belongsToMany(GeoPosition::class, 'quest_has_points', 'geo_quest_id', 'geo_position_id')
-            ->withTimestamps();
+        return $this->belongsToMany(GeoPosition::class);
     }
 }
