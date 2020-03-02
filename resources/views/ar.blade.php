6de10aaff398568632a8647de5f97bda21e123a7
exxxar <exxxar@gmail.com>
2020-03-02 22:15:18 +0300

    добавление квестов в бота

--- resources/views/ar.blade.php
+++ resources/views/ar.blade.php
@@ -1,25 +1,154 @@
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-
-    <script src="https://aframe.io/releases/0.6.0/aframe.min.js"></script>
-    <script src="https://jeromeetienne.github.io/AR.js/aframe/build/aframe-ar.js"></script>
+    <meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
+    <title>Hello, world!</title>
+    <!-- include three.js library -->
+    <script src='https://stemkoski.github.io/AR-Examples/js/three.js'></script>
+    <!-- include jsartookit -->
+    <script src="https://stemkoski.github.io/AR-Examples/jsartoolkit5/artoolkit.min.js"></script>
+    <script src="https://stemkoski.github.io/AR-Examples/jsartoolkit5/artoolkit.api.js"></script>
+    <!-- include threex.artoolkit -->
+    <script src="https://stemkoski.github.io/AR-Examples/threex/threex-artoolkitsource.js"></script>
+    <script src="https://stemkoski.github.io/AR-Examples/threex/threex-artoolkitcontext.js"></script>
+    <script src="https://stemkoski.github.io/AR-Examples/threex/threex-arbasecontrols.js"></script>
+    <script src="https://stemkoski.github.io/AR-Examples/threex/threex-armarkercontrols.js"></script>
 </head>
 
-<body style='margin : 0px; overflow: hidden;'>
-<a-scene embedded arjs='sourceType: webcam;'>
+<body style='margin : 0px; overflow: hidden; font-family: Monospace;'>
+
+<!-- 
+  Example created by Lee Stemkoski: https://github.com/stemkoski
+  Based on the AR.js library and examples created by Jerome Etienne: https://github.com/jeromeetienne/AR.js/
+-->
+
+<script>
+
+    var scene, camera, renderer, clock, deltaTime, totalTime;
+
+    var arToolkitSource, arToolkitContext;
+
+    var markerRoot1;
+
+    var mesh1;
+
+    initialize();
+    animate();
+
+    function initialize()
+    {
+        scene = new THREE.Scene();
+
+        let ambientLight = new THREE.AmbientLight( 0xcccccc, 0.5 );
+        scene.add( ambientLight );
+
+        camera = new THREE.Camera();
+        scene.add(camera);
+
+        renderer = new THREE.WebGLRenderer({
+            antialias : true,
+            alpha: true
+        });
+        renderer.setClearColor(new THREE.Color('lightgrey'), 0)
+        renderer.setSize( 640, 480 );
+        renderer.domElement.style.position = 'absolute'
+        renderer.domElement.style.top = '0px'
+        renderer.domElement.style.left = '0px'
+        document.body.appendChild( renderer.domElement );
+
+        clock = new THREE.Clock();
+        deltaTime = 0;
+        totalTime = 0;
+
+        ////////////////////////////////////////////////////////////
+        // setup arToolkitSource
+        ////////////////////////////////////////////////////////////
+
+        arToolkitSource = new THREEx.ArToolkitSource({
+            sourceType : 'webcam',
+        });
+
+        function onResize()
+        {
+            arToolkitSource.onResize()
+            arToolkitSource.copySizeTo(renderer.domElement)
+            if ( arToolkitContext.arController !== null )
+            {
+                arToolkitSource.copySizeTo(arToolkitContext.arController.canvas)
+            }
+        }
+
+        arToolkitSource.init(function onReady(){
+            onResize()
+        });
+
+        // handle resize event
+        window.addEventListener('resize', function(){
+            onResize()
+        });
+
+        ////////////////////////////////////////////////////////////
+        // setup arToolkitContext
+        ////////////////////////////////////////////////////////////
+
+        // create atToolkitContext
+        arToolkitContext = new THREEx.ArToolkitContext({
+            cameraParametersUrl: 'https://stemkoski.github.io/AR-Examples/data/camera_para.dat',
+            detectionMode: 'mono'
+        });
+
+        // copy projection matrix to camera when initialization complete
+        arToolkitContext.init( function onCompleted(){
+            camera.projectionMatrix.copy( arToolkitContext.getProjectionMatrix() );
+        });
+
+        ////////////////////////////////////////////////////////////
+        // setup markerRoots
+        ////////////////////////////////////////////////////////////
+
+        // build markerControls
+        markerRoot1 = new THREE.Group();
+        scene.add(markerRoot1);
+        let markerControls1 = new THREEx.ArMarkerControls(arToolkitContext, markerRoot1, {
+            type: 'pattern', patternUrl: "https://skidka-service.ru/1.patt",
+        })
+
+        let geometry1 = new THREE.PlaneBufferGeometry(1,1, 4,4);
+        let loader = new THREE.TextureLoader();
+        let texture = loader.load( 'https://skidka-service.ru/1.png', render );
+        let material1 = new THREE.MeshBasicMaterial( { map: texture } );
+
+        mesh1 = new THREE.Mesh( geometry1, material1 );
+        mesh1.rotation.x = -Math.PI/2;
+
+        markerRoot1.add( mesh1 );
+    }
 
 
-    <a-marker preset="pattern" type="pattern" url="http://skidka-service.ru/1.patt">
-        <a-box position='0 0.5 0' material='color: black;' soundhandler></a-box>
-    </a-marker>
-    <!-- add a simple camera -->
-    <a-entity camera></a-entity>
-</a-scene>
+    function update()
+    {
+        // update artoolkit on every frame
+        if ( arToolkitSource.ready !== false )
+            arToolkitContext.update( arToolkitSource.domElement );
+    }
+
+
+    function render()
+    {
+        renderer.render( scene, camera );
+    }
+
+
+    function animate()
+    {
+        requestAnimationFrame(animate);
+        deltaTime = clock.getDelta();
+        totalTime += deltaTime;
+        update();
+        render();
+    }
+
+</script>
+
 </body>
 </html>
