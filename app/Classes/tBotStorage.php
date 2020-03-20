12346cf9fc953eb737ccfee916a2af9e0b589c4d
exxxar <exxxar@gmail.com>
2020-03-20 22:14:11 +0300

    правки по тонкому клиенту

--- app/Classes/tBotStorage.php
+++ app/Classes/tBotStorage.php
@@ -12,7 +12,6 @@ trait tBotStorage
 
     public function addToStorage($key, $value)
     {
-        Log::info($this->telegram_user->id);
         $tmp = json_decode(Cache::get($this->telegram_user->id, "[]"), true);
         if (!array_key_exists("$key",$tmp)) {
             array_push($tmp, ["$key" => $value]);
@@ -26,7 +25,6 @@ trait tBotStorage
             Cache::forget($this->telegram_user->id);
             Cache::add($this->telegram_user->id, json_encode($tmp));
         }
-        Log::info($this->telegram_user->id);
         Log::info(Cache::get($this->telegram_user->id,'empty'));
     }
 
@@ -40,6 +38,8 @@ trait tBotStorage
     public function getFromStorage($key, $default)
     {
         $tmp = json_decode(Cache::get($this->telegram_user->id, "[]"), true);
+
+        Log::info(Cache::get($this->telegram_user->id,'empty'));
 
         return array_key_exists("$key",$tmp) ? $tmp[$key] : $default;
     }
